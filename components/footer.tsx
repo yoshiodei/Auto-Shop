640cfe0210e381e909806e40c41eb6c9035851a7
Yoshi Odei <yoshiodei@yahoo.com>
2026-05-31 09:49:32 +0000

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="w-full flex items-center justify-center gap-2 p-4 bg-white border-t border-gray-200">
+    <div className="w-full flex items-center justify-center gap-2 p-4 bg-white border-t border-gray-200 mt-10">
         <p className="text-gray-600 text-sm">
             Made with love by Kazu
         </p>
