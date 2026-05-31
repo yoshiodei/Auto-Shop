eb71eba00b5281a9580e0911fe33b19d9b45ee02
Yoshi Odei <yoshiodei@yahoo.com>
2026-05-31 09:52:39 +0000

    minor code edit

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="w-full flex items-center justify-center gap-2 p-4 bg-white border-t border-gray-200 mt-10">
-        <p className="text-gray-600 text-sm">
+        <p className="text-gray-300 text-sm">
             Made with love by Kazu
         </p>
     </div>
